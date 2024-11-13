2ac462dd9432518e49653211a5bdbf9dfa9903ac
dooyong <dooyong86@gmail.com>
2024-11-13 13:48:19 +0900

    jwt

--- src/main/java/com/example/springjwt/jwt/JWTUtil.java
+++ src/main/java/com/example/springjwt/jwt/JWTUtil.java
@@ -17,6 +17,10 @@ public class JWTUtil {
         this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
 
+    public String getCategory(String token){
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("category", String.class);
+    }
+
     public String getUsername(String token) {
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
     }
@@ -29,8 +33,9 @@ public class JWTUtil {
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
     }
 
-    public String createJwt(String username, String role, long expiredMs) {
+    public String createJwt(String category, String username, String role, long expiredMs) {
         return Jwts.builder()
+                .claim("category", category)
                 .claim("username", username)
                 .claim("role", role)
                 .issuedAt(new Date(System.currentTimeMillis()))
